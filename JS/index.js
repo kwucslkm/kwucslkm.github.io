db7b9cef8d98006e41c4b9012f4787f0c2b65d25
kwucslkm <kwucsani@naver.com>
2023-04-18 16:42:36 +0900

    commit 20

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,14 @@
 const fun1 = () => {
     alert(" 만든이 : 이광명");
-
-
 }
 const goMemreg = () => {
     location.href = "06_memberReg.html";
-    // window.open("06_memberReg.html");
-
+    // window.open("06_memberReg.html","_black");
 }
 // const goIndex = () => {
 //     history.back();
 // }
+const gojcgi = () => {
+    location.href = "https://www.notion.so/bb6d2273dfca4b07a338fd35f6752f76?pvs=4";
+    // window.open("06_memberReg.html");
+}
